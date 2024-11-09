72444e0dfc580cd09be1ed3a06f5bb545ff75b87
hyeri00 <nexusring@gmail.com>
2024-11-10 01:13:56 +0900

    initial commit

---
+++ myHouseholdLedger/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  myHouseholdLedger
+//
+//  Created by 혜리 on 11/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ myHouseholdLedger/myHouseholdLedgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  myHouseholdLedgerApp.swift
+//  myHouseholdLedger
+//
+//  Created by 혜리 on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct myHouseholdLedgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
